b36c43457084a4b8386155fdc25bd9eb2784c2ac
ye <amazingye@live.com>
2017-03-11 01:24:31 +0800

    UI redesign for the third time

---
+++ app/src/main/java/com/ye/deertutor/Activities/TeacherDetailActivity.java
@@ -0,0 +1,13 @@
+package com.ye.deertutor.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeacherDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_detail);
+    }
+}
---
+++ app/src/main/java/com/ye/deertutor/Adapter/TeacherAdapter.java
@@ -0,0 +1,8 @@
+package com.ye.deertutor.Adapter;
+
+/**
+ * Created by X1 Carbon on 2017/3/5.
+ */
+
+public class TeacherAdapter {
+}
