80997797b8c4d30fcfe1b0c71274cbd444c3d5ef
ye <amazingye@live.com>
2017-03-21 22:40:23 +0800

    Bmob IM SDK added

---
+++ app/src/main/java/com/ye/deertutor/DeerMessageHandler.java
@@ -0,0 +1,10 @@
+package com.ye.deertutor;
+
+import cn.bmob.newim.listener.BmobIMMessageHandler;
+
+/**
+ * Created by X1 Carbon on 2017/3/20.
+ */
+
+public class DeerMessageHandler extends BmobIMMessageHandler {
+}
